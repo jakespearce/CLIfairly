79cef75c19c52c7a88b2baafa2668c8363750741
jsp <jakespearce@gmail.com>
2016-03-21 22:43:08 +0000

    scrolly menu system

--- menu_files/pokedex_files/pokedex.sh
+++ menu_files/pokedex_files/pokedex.sh
@@ -11,6 +11,12 @@ hiOff=$( tput rmso  )
 
 where_selection_is=1
 
+# for explanations on what these poorly named variables are see line 106.
+# tl;dr the value of F determines how many menu items a player sees at any given time. we only show a small window of menu items at any given time because showing all 151 pokedex menu lines at once would be silly.
+B=1
+
+F=7
+
 # could hard code 151 BUT WE GOTTA THINK OF THE SEQUEL BABY
 pokedex_height=$( wc -l < "${pokedex_files}/pokedex"  )
 
@@ -35,34 +41,36 @@ refresh_pokedex(){
 
 		((count++))
 
+	# the statements below 'modify' how the pokemon will be displayed to the player. Remember the only thing the player actually sees is the /dev/shm/marked_pokedex file.
+
 		# for when that item is 'selected'
 		if [ "$count" -eq "$where_selection_is" ]; then
 			# if we've not seen the pokemon before and not caught it
 			if [ "$seen" -eq 0 -a "$own" -eq 0 ]; then
-				echo "---------" "selected"
+				echo $id "---------" "selected"
 			
 			# if we've seen the pokemon before and not caught it
 			elif [ "$seen" -eq 1 -a "$own" -eq 0 ]; then
-				echo $name "selected"
+				echo $id $name "selected"
 
 			# if we've seen the pokemon before and caught it
 			else
-				echo "${name}*" "selected"
+				echo $id "${name}*" "selected"
 			fi
 
 		# for when an item is not 'selected'
 		else
             # if we've not seen the pokemon before and not caught it
             if [ "$seen" -eq 0 -a "$own" -eq 0 ]; then
-                echo "---------"
+                echo $id "---------"
                 
             # if we've seen the pokemon before and not caught it
             elif [ "$seen" -eq 1 -a "$own" -eq 0 ]; then
-                echo $name
+                echo $id $name
 
             # if we've seen the pokemon before and caught it
             else
-                echo "${name}*"
+                echo $id "${name}*"
 			fi
 		fi
 	
@@ -73,16 +81,44 @@ refresh_pokedex(){
 
 show_pokedex(){
 
-	while read name selected; do
+	reposition_window
+
+	local count=0
+
+		while read id name selected; do
 		
-		if [ "$selected" == "selected"  ]; then
-			echo "$hiOn $name $hiOff"
-		else
-			echo "$name"
-		fi
+			((count++))
+		
+			if [ "$count" -ge "$B" -a $count -le "$F"  ]; then
+	
+				if [ "$selected" == "selected"  ]; then
+					echo "$hiOn $id $name $hiOff"
+				else
+					echo "$id $name"
+				fi
 
-	done < /dev/shm/marked_pokedex
+			fi
 
+		done < /dev/shm/marked_pokedex
+
+}
+
+
+# B = Before where_selection_is
+# F = in Front of where_selection_is
+# This function is used in show_pokedex so that the player only sees 7 menu items at a time. How 7? Because we initally set F=7 and B=0
+# B to F represents a range of menu lines that the player sees. This range is primarily controlled by $where_selection_is. 
+# reposition_window shifts our visible range of menu items up or down depending in which direction $where_selection_is drags it.
+reposition_window(){
+
+	if [ "$where_selection_is" -lt "$B" -a "$B" -ne 1 ]; then
+		B=$(( $B - 1 ))
+		F=$(( $F - 1 ))
+	
+	elif [ "$where_selection_is" -gt "$F" -a "$F" -ne "$pokedex_height" ]; then
+		B=$(( $B + 1  ))
+		F=$(( $F + 1 ))
+	fi
 }
 
 

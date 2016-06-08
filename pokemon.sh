215451470b701e09d95eacce17670b8d70afcb56
jakespearce <jakespearce@gmail.com>
2016-06-08 23:44:24 +0100

    ew spaces

--- pokemon.sh
+++ pokemon.sh
@@ -11,18 +11,18 @@ source "${pokemon_path}/tools/tools.sh"
 
 input_prompt(){
 
-    read -n1 input
-    case $input in
-        w) up ;;
-        s) down ;;
-        a) left ;;
-        d) right ;;
-        # interaction is a function found whatever the current_map_functions file is set to
+	read -n1 input
+	case $input in
+		w) up ;;
+		s) down ;;
+		a) left ;;
+		d) right ;;
+		# interaction is a function found whatever the current_map_functions file is set to
 		# map_rw_path needs to be located in /dev/shm in the future
-        e) clear ; cat "${map_rw_path}/marked_map_output" ; echo "" ; echo -n " " ; interaction ;;
-        m) bash ${HOME}/pokemon/gui/menu_files/menu.sh ; cat "${map_rw_path}/marked_map_output" ;;
-        *) echo "WASD to move, e to interact with things, m for menu." ;;
-    esac
+		e) clear ; cat "${map_rw_path}/marked_map_output" ; echo "" ; echo -n " " ; interaction ;;
+		m) bash ${HOME}/pokemon/gui/menu_files/menu.sh ; cat "${map_rw_path}/marked_map_output" ;;
+		*) echo "WASD to move, e to interact with things, m for menu." ;;
+	esac
 }
 
 
